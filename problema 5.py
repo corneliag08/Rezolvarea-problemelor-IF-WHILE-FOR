2d1cc63b14a49ef09872271093e5f7cd49d3352b
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2020-11-19 11:47:44 +0300

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,27 @@
+a=eval(input("Anul : "))
+if (a//1000==0):
+    print("Introdu alt an!")
+elif (a%12==0):
+    print("Anul maimuței")
+elif (a%12==1):
+    print("Anul cucoșului")
+elif (a%12==2):
+    print("Anul câinelui")
+elif (a%12==3):
+    print("Anul porcului")
+elif (a%12==4):
+    print("Anul sobolanului")
+elif (a%12==5):
+    print("Anul boului")
+elif (a%12==6):
+    print("Anul tigrului")
+elif (a%12==7):
+    print("Anul iepurelui")
+elif (a%12==8):
+    print("Anul dragonului")
+elif (a%12==9):
+    print("Anul sarpelui")
+elif (a%12==10):
+    print("Anul calului")
+else:
+    print("Anul oaiei")
